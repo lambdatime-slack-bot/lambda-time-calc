140865465759f81366dbc23786f103c3643f445c
josh182014 <josh182014@gmail.com>
2019-07-01 16:43:52 -0600

    changed user error message status code to 200 as per slacks

--- slack/slack-router.js
+++ slack/slack-router.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
         }
     }
     else {
-        res.status(422).json({text: "Please check format. Input should be shaped like this: 12:00 pm edt"})
+        res.status(200).json({text: "Please check format. Input should be shaped like this: 12:00 pm edt"})
     }
 })
 
